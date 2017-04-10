63f084a8aa9dce2a3f1bd960353360f5e1aec366
jnprogrammer <joshuanparker@gmail.com>
2017-04-10 06:15:07 -0400

    Updated SPA 2.1
    Part of the SPA 2 commit

--- public/angular-app/hotel-display/hotel-display-ctrl.js
+++ public/angular-app/hotel-display/hotel-display-ctrl.js
@@ -1,6 +1,9 @@
 angular.module('meanhotel').controller('hotelController',hotelController);
+console.log("DId this work? hotel display controller");
+function hotelController($routeParams,hotelDataFactory){
     var vm = this;
     var id = $routeParams.id;
+    hotelDataFactory.hotelDisplay(id).then(function(response){
         vm.hotel = response.data
     });
 }
